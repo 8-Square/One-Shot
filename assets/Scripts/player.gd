a8c3fdede248c51fbb0f0f5a9f49b7bc9446d164
4_Square <104998118+8-Square@users.noreply.github.com>
2025-07-21 10:55:49 +1000

    Working Bounce
    Now bounce doesnt activate when jumping and only works when the player hits the walls

--- assets/Scripts/player.gd
+++ assets/Scripts/player.gd
@@ -1,11 +1,12 @@
 extends CharacterBody2D
 
-
 var SPEED = 500.0
 const JUMP_VELOCITY = -700.0
 
 
 func _physics_process(delta: float) -> void:
+	
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -19,13 +20,21 @@ func _physics_process(delta: float) -> void:
 	var direction := Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
-	elif is_on_wall(): 
-		velocity.x *= -1
-		velocity.x = SPEED
-	elif not is_on_wall():
-		velocity.x = move_toward(velocity.x, 0, 0)
+	#elif is_on_wall(): 
+		#velocity.x *= -1
+		#velocity.x = SPEED
+	#elif not is_on_wall():
+		#velocity.x = move_toward(velocity.x, 0, 0)
 	else:
 		velocity.x = move_toward(velocity.x, 0, 0)
 		
+	var collision_info = move_and_collide(velocity * delta)
 
+	if collision_info:
+
+		if is_on_floor():
+			var direction2 = velocity.bounce(collision_info.get_normal())
+			velocity = direction2
+		print(velocity)
+		
 	move_and_slide()
