b9b502dfe6631eb0f847f419d3c393294ced2a33
4_Square <104998118+8-Square@users.noreply.github.com>
2025-07-21 19:25:07 +1000

    Split Tutorial and Level One Scripts

--- assets/Scripts/player_LevelOne.gd
+++ assets/Scripts/player_LevelOne.gd
@@ -2,13 +2,12 @@ extends CharacterBody2D
 
 var SPEED = 400.0
 const JUMP_VELOCITY = -700.0
-const wall_jump = 1000
 var right_count = 0
 var left_count = 0
 var jump_count = 0
 
 func _physics_process(delta: float) -> void:
-	
+
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -16,16 +15,30 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		jump_count += 1
+		print("jump is equal to " + str(jump_count))
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
+		if Input.is_action_just_pressed("left"): 
+			left_count += 1
+			print("Left Count is " + str(left_count))
+		elif Input.is_action_just_pressed("right"):
+			right_count += 1
+			print("Right Count is " + str(right_count))
+		if is_on_wall():
+			velocity.x *= -1
+			velocity.x = SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, 0)
+
 	else:
 		velocity.x = move_toward(velocity.x, 0, 0)
 	
-	var collision_info = move_and_collide(velocity * delta, false, 0.6, true)
+	var collision_info = move_and_collide(velocity * delta, false, 0.3, true)
 
 	if collision_info:
 		var direction2 = velocity.bounce(collision_info.get_normal())
@@ -33,3 +46,5 @@ func _physics_process(delta: float) -> void:
 			velocity = direction2
 		
 	move_and_slide()
+
+ 
---
+++ assets/Scripts/player_tutorial.gd
@@ -0,0 +1,36 @@
+extends CharacterBody2D
+
+
+var SPEED = 400.0
+const JUMP_VELOCITY = -700.0
+
+
+func _physics_process(delta: float) -> void:
+
+
+		
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+		
+	var direction := Input.get_axis("left", "right")
+	if direction:
+		velocity.x = direction * SPEED
+	elif is_on_wall(): 
+		velocity.x *= -1
+		velocity.x = SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, 0)
+		
+	var collision_info = move_and_collide(velocity * delta, false, 0.3, false)
+
+	if collision_info:
+		var direction2 = velocity.bounce(collision_info.get_normal())
+		if is_on_floor():
+			velocity = direction2
+		print(velocity)
+		
+	move_and_slide()
