485919f42cf3cfe1284299fb70955e0cc75bcb8f
4_Square <104998118+8-Square@users.noreply.github.com>
2025-07-21 15:28:09 +1000

    Starting limited counts

--- assets/Scripts/player.gd
+++ assets/Scripts/player.gd
@@ -1,11 +1,13 @@
 extends CharacterBody2D
 
-var SPEED = 500.0
+var SPEED = 400.0
 const JUMP_VELOCITY = -700.0
-
+const wall_jump = 1000
+var right_count = 0
+var left_count = 0
+var jump_count = 0
 
 func _physics_process(delta: float) -> void:
-	
 	
 	# Add the gravity.
 	if not is_on_floor():
@@ -20,21 +22,14 @@ func _physics_process(delta: float) -> void:
 	var direction := Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
-	#elif is_on_wall(): 
-		#velocity.x *= -1
-		#velocity.x = SPEED
-	#elif not is_on_wall():
-		#velocity.x = move_toward(velocity.x, 0, 0)
 	else:
 		velocity.x = move_toward(velocity.x, 0, 0)
-		
-	var collision_info = move_and_collide(velocity * delta)
+	
+	var collision_info = move_and_collide(velocity * delta, false, 0.6, true)
 
 	if collision_info:
-
-		if is_on_floor():
-			var direction2 = velocity.bounce(collision_info.get_normal())
+		var direction2 = velocity.bounce(collision_info.get_normal())
+		if is_on_floor() or is_on_wall():
 			velocity = direction2
-		print(velocity)
 		
 	move_and_slide()
