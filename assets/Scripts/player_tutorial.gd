4f4b8679652fc5463360480bac50e3f21a83899c
4_Square <104998118+8-Square@users.noreply.github.com>
2025-07-22 14:07:05 +1000

    Building more of Tutorial

--- assets/Scripts/player_tutorial.gd
+++ assets/Scripts/player_tutorial.gd
@@ -1,14 +1,11 @@
 extends CharacterBody2D
 
 
-var SPEED = 400.0
-const JUMP_VELOCITY = -700.0
+var SPEED = 500.0
+const JUMP_VELOCITY = -600.0
 
 
 func _physics_process(delta: float) -> void:
-
-
-		
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
@@ -29,7 +26,7 @@ func _physics_process(delta: float) -> void:
 
 	if collision_info:
 		var direction2 = velocity.bounce(collision_info.get_normal())
-		if is_on_floor():
+		if is_on_floor() or is_on_wall():
 			velocity = direction2
 		print(velocity)
 		
