a8a50d34e3ddb1d78237ca972694822d444b107b
4_Square <104998118+8-Square@users.noreply.github.com>
2025-10-18 15:06:25 +1000

    Parallax Background for Tutorial
    also reorganizing files

--- fade_area.gd
+++ fade_area.gd
@@ -4,7 +4,7 @@ func _on_body_entered(body: Node2D) -> void:
 	if body is BasePlayer:
 		var parent = self.get_parent()
 		if parent:
-			parent.modulate.a = 0.1
+			parent.modulate.a = 0.2
 
 func _on_body_exited(body: Node2D) -> void:
 	if body is BasePlayer:
