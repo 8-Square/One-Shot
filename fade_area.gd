288adf93b5fdf198a07f69110170d1d671d003fb
4_Square <104998118+8-Square@users.noreply.github.com>
2025-10-18 13:36:45 +1000

    shhh hidden area
    finally the hidden areas are functional
    
    and also made the text in unison throughout the levels

---
+++ fade_area.gd
@@ -0,0 +1,13 @@
+extends Area2D
+
+func _on_body_entered(body: Node2D) -> void:
+	if body is BasePlayer:
+		var parent = self.get_parent()
+		if parent:
+			parent.modulate.a = 0.1
+
+func _on_body_exited(body: Node2D) -> void:
+	if body is BasePlayer:
+		var parent = self.get_parent()
+		if parent:
+			parent.modulate.a = 1
