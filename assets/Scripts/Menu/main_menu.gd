5b9b319089f72a368aa41a343b9d3ad831abf484
4_Square <104998118+8-Square@users.noreply.github.com>
2025-12-09 22:12:08 +1000

    Fixing another small issue

--- assets/Scripts/Menu/main_menu.gd
+++ assets/Scripts/Menu/main_menu.gd
@@ -1,23 +1,28 @@
 class_name MainMenu extends Control
 
+@onready var canvas_layer: CanvasLayer = $CanvasLayer
+
 func _ready() -> void:
 	$CanvasLayer/MarginContainer/VBoxContainer/Play.grab_focus()
 
 func _on_play_pressed() -> void:
-	hide()
+	canvas_layer.hide()
 	$UISelect.play()
 	to_tutorial_transition.change_scene(to_tutorial_transition.levelselect)
 
 func _on_settings_pressed() -> void:
+	canvas_layer.hide()
 	$UISelect.play()
 	to_tutorial_transition.change_scene(to_tutorial_transition.settings)
 
 
 func _on_quit_pressed() -> void:
+	canvas_layer.hide()
 	$QuitSound.play()
 	await get_tree().create_timer(0.3).timeout
 	get_tree().quit()
 
 func _on_credits_pressed() -> void:
+	canvas_layer.hide()
 	$UISelect.play()
 	to_tutorial_transition.change_scene(to_tutorial_transition.credits)
