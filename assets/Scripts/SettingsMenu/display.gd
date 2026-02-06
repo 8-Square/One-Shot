e08211ac8c8c05846e4eb413be3c85c22aca30a7
4_Square <104998118+8-Square@users.noreply.github.com>
2026-02-06 19:50:16 +1000

    Fixed VSync
    For real this time

--- assets/Scripts/SettingsMenu/display.gd
+++ assets/Scripts/SettingsMenu/display.gd
@@ -10,7 +10,7 @@ class_name DisplaySettings extends Control
 @onready var fps_label: Label = $VBoxContainer/FPSBox/FPSLabel
 
 @onready var vsync_text: Label = $VBoxContainer/VSyncBox/VSyncText
-
+var vsync_toggle: bool 
 func _ready() -> void:
 	Engine.max_fps = 0
 	_on_v_sync_button_toggled(true)
@@ -33,8 +33,8 @@ func fps_preview() -> void:
 
 # VSync Toggle
 func _on_v_sync_button_toggled(enabled: bool) -> void:
-	vsync_text.text = "On" if enabled == true else "Off"
-
+	vsync_text.text = "On" if enabled else "Off"
+	vsync_toggle = true if enabled else false
 
 	## If toggled on, enable VSync
 	#if enabled:
@@ -57,7 +57,12 @@ func save_settings():
 	# Checks for fps value. 
 	# if below 365 then it = to slider ||| if above 365 (unlimited), don't cap fps 
 	if fps_slider.value < 155.0:
+		@warning_ignore("narrowing_conversion")
 		Engine.max_fps = fps_slider.value
 	else: 
 		Engine.max_fps = 0
 	print("Display Settings Successfully Saved")
+	print(str(vsync_toggle))
+	# Sets VSync mode
+	var vsync_mode = DisplayServer.VSYNC_ENABLED if vsync_toggle == true else DisplayServer.VSYNC_DISABLED
+	DisplayServer.window_set_vsync_mode(vsync_mode)
